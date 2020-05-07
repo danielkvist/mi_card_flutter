2181cfaf16f1826aebec83b86fcd9694410389c9
Danielkvist <danielkvist@protonmail.com>
2020-05-07 11:37:32 +0200

    refactor MaterialApp into a stateless widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,24 @@
 import 'package:flutter/material.dart';
 
+// In order to hot reload to work better
+// we need a stateless or statefull widget.
+// So our hot reload will only rebuild
+// the modified widget instead the complete app
+// using the build method inside that widget.
+// Hot reload then will save the state of the not modified
+// widgets for you.
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.teal,
+        backgroundColor: Colors.blue,
         body: Container(),
       ),
-    ),
-  );
+    );
+  }
 }
